e178930d03f52e47f02a9fbfd542e5e140d9b318
OscarLVz <olechugav1311@gmail.com>
2023-03-14 19:47:57 -0600

    Timer updated

--- src/app/components/timer/timer/timer.component.ts
+++ src/app/components/timer/timer/timer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-timer',
+  selector: 'timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
