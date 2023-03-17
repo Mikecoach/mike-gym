499af0eb4f76711f6270fb1c653a4617fff3ee54
OscarLVz <olechugav1311@gmail.com>
2023-03-16 22:41:22 -0600

    Timer ok

--- src/app/components/timer/timer/timer.component.ts
+++ src/app/components/timer/timer/timer.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit {
 
+  step: string = "";
+  time: number = 0;
+  prepare: number = 5;
+  work: number = 3;
+  rest: number = 5;
+  cycles: number = 2;
+  sets: number = 2;
+  restBetweenSets: number = 6;
+  coolDown: number = 20;
+
+  sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+  ngOnInit(): void {
+    this.start();
+  }
+
+  /**
+   * Start job
+   */
+  async start() {
+    //Prepare
+    await this.timer("Preparación", this.prepare);
+    //Sets
+    for (let index = 1; index <= this.sets; index++) {
+      //Cicles
+      for (let index = 1; index <= this.cycles; index++) {
+        //Work
+        await this.timer("Ciclo " + index + " - Trabajo", this.work);
+        //Rest
+        if (index < this.cycles) {
+          await this.timer("Ciclo #" + index + " - Descanso", this.rest);
+        }
+      }
+      //Rest between sets
+      if (index < this.sets) {
+        await this.timer("Descanso entre set #" + index, this.restBetweenSets);
+      }
+    }
+    //Cool down
+    await this.timer("Descanso final", this.coolDown);
+  }
+
+  /**
+   * Timer count
+   * @param title step title
+   * @param time seconds
+   */
+  async timer(title: string, time: number) {
+    this.step = title;
+    this.time = time;
+    while (this.time > 0) {
+      await this.sleep(1000);
+      this.time--;
+    }
+  }
 }
